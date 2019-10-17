9990447b5faa182fcbce057470b1ae9e3ea09972
Claire <mcgicla@live.com>
2019-10-18 10:15:57 +1100

    homepage and geo-graphics page

---
+++ local-area-network/assets/js/main.js
@@ -0,0 +1,79 @@
+$ (document).ready(function(){
+	console.log("The page is ready!");
+
+	// $(".research-item").click(function(){
+	// 	$(this).toggleClass('large');
+	// });
+
+	$('.months-header').click(function(){
+		$('.months-header').removeClass('active');
+		$(this).addClass('active');
+	})
+
+	var hideAll = function() {
+		$(".thumbnail").addClass('hide');
+	}
+
+	$('.btn-jan').click(function(){
+		hideAll();
+		$('.jan').removeClass('hide');
+	});
+
+	$('.btn-feb').click(function(){
+		hideAll();
+		$('.feb').removeClass('hide');
+	});
+
+	$('.btn-mar').click(function(){
+		hideAll();
+		$('.mar').removeClass('hide');
+	});
+
+	$('.btn-apr').click(function(){
+		hideAll();
+		$('.apr').removeClass('hide');
+	});
+
+	$('.btn-may').click(function(){
+		hideAll();
+		$('.may').removeClass('hide');
+	});
+
+	$('.btn-jun').click(function(){
+		hideAll();
+		$('.jun').removeClass('hide');
+	});
+
+	$('.btn-jul').click(function(){
+		hideAll();
+		$('.jul').removeClass('hide');
+	});
+
+	$('.btn-aug').click(function(){
+		hideAll();
+		$('.aug').removeClass('hide');
+	});
+
+	$('.btn-sep').click(function(){
+		hideAll();
+		$('.sep').removeClass('hide');
+	});
+
+	$('.btn-oct').click(function(){
+		hideAll();
+		$('.oct').removeClass('hide');
+	});
+
+	$('.btn-nov').click(function(){
+		hideAll();
+		$('.nov').removeClass('hide');
+	});
+
+	$('.btn-dec').click(function(){
+		hideAll();
+		$('.dec').removeClass('hide');
+	});
+
+
+
+});
